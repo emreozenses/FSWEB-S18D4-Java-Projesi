09886c56d0597df38884fca19a3849dfbe3bf04d
Emre OZENSES <emreozenses@gmail.com>
2024-01-31 21:25:10 +0300

    task_2 entity layer completed

---
+++ src/main/java/com/workintech/FSWEBS18D4JavaProjesi/entity/Address.java
@@ -0,0 +1,40 @@
+package com.workintech.FSWEBS18D4JavaProjesi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Address",schema = "fsweb")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "no")
+    private int no;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToOne(cascade = {CascadeType.DETACH ,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH},mappedBy = "address")
+    private Customer customer;
+
+
+
+}
